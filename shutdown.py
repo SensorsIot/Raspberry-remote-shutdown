1a33c89b2eeffd6e9d034f3634df73adc1f8a9ed
sensorsiot <andreas.spiess@arumba.com>
2020-10-08 22:11:37 +0200

    Added credentials

--- shutdown.py
+++ shutdown.py
@@ -4,6 +4,11 @@ import sys
 import paho.mqtt.client as mqtt
 import time
 broker="192.168.0.203"
+
+auth = {
+  'username':"admin",
+  'password':"admin"
+}
 #define callback
 def on_message(client, userdata, message):
    msg=str(message.payload.decode("utf-8"))
@@ -13,7 +18,7 @@ def on_message(client, userdata, message):
 def on_connect(client, userdata, flags,rc):
     if rc==0:
         client.connected_flag=True
-        client.subscribe(sub_topic)
+        client.subscribe(sub_topic, auth=auth)
     else:
         client.bad_connection_flag=True
         client.connected_flag=False
@@ -33,5 +38,5 @@ while True:
    if len(messages)>0:
       m=messages.pop(0)
       print("shutdown")
-      client.publish("prusa/mains/sonoff", payload=None, qos=0, retain=False)
+      client.publish("prusa/mains/sonoff", payload=None, auth=auth, qos=0, retain=False)
       os.system("sudo shutdown now")
